c44929c1bd8e34356f5114c64ff846145828a4d9
Romain9 <romain.msiah@gmail.com>
2021-11-14 15:31:05 +0100

    Index.php + folder(Style.css)

--- index.php
+++ index.php
@@ -1,1 +1,39 @@
 Online
+
+<!DOCTYPE html>
+<html lang="fr">
+    <meta charset = "UTF-8">
+    <title>RockInfo - Accueil</title>   <!-- Titre onglet  -->
+</html>
+<head>
+    <link rel="stylesheet" href="style.css"> <!-- Liens vers fiche de style  -->
+</head>
+<body>
+
+    <h1></h1>
+
+    <h2 class="module">Rock Info</h2>
+
+    <h1></h1>
+
+    <div class="navbar">                  <!-- Barre de navigation + contenue  -->
+        <a href="Index.php">Accueil</a>
+        <a href="FAQ.php">FAQ</a>
+        <a href="Store.php">Boutique</a>
+        <a href="Event.php">Futurs événements</a>
+        <div class="dropdown">
+            <button class="dropbtn">Compte
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+                <a href="#">Inscription/Connexion</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+
+
+
