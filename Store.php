5689687853c60cccb4e713fb8f9044ef197ece3f
Romain9 <romain.msiah@gmail.com>
2021-11-14 16:16:39 +0100

    Fond + pages(event, store, FAQ)

---
+++ Store.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <meta charset = "UTF-8">
+    <title>RockInfo - Boutique</title>   <!-- Titre onglet  -->
+</html>
+<head>
+    <link rel="stylesheet" href="style.css"> <!-- Liens vers fiche de style  -->
+</head>
+<body>
+
+    <h1></h1>
+
+    <h2 class="module">Rock Info</h2>
+
+    <h1></h1>
+
+    <div class="navbar">                  <!-- Barre de navigation + contenue  -->
+        <a href="index.php">Accueil</a>
+        <a href="FAQ.php">FAQ</a>
+        <a href="Store.php">Boutique</a>
+        <a href="Event.php">Futurs événements</a>
+        <div class="dropdown">
+            <button class="dropbtn">Compte
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+                <a href="#">Inscription/Connexion</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+
+
