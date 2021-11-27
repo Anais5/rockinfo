be2d12bb43c3b73186556e60408c50ce9d8bd3bf
Romain9 <romain.msiah@gmail.com>
2021-11-27 18:45:17 +0100

    Footer, progression boutique (mais pb d'affichage) et déplacement balise table

--- php/header.php
+++ php/header.php
@@ -9,6 +9,8 @@
     <link rel="stylesheet" href="css/styles.css"> <!-- Liens vers fiche de style  -->
     <link rel="stylesheet" href="css/switch_log.css">
     <link rel="stylesheet" href="css/cadreForm.css">
+    <link rel="stylesheet" href="css/footerStyle.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <h2 class="module">Rock Info</h2>
