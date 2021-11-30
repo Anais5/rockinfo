bc5fbfb42d69be5909c732882a06b5a1a5ab0dea
Anais5 <anaiscolrat@gmail.com>
2021-11-30 14:12:00 +0100

    modif fond + logo adaptatif et changement couleur footer

--- php/header.php
+++ php/header.php
@@ -13,4 +13,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-    <h2 class="module">Rock Info</h2>
+    <div class="centrer">
+    <img src="pics/LOGO.png" class="logo">
+    </div>
